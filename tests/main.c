57e7f7819d95b2e8f7d2ff7686c87c0bd27e5134
stargateaudio <stargateaudio@noreply.github.com>
2020-11-29 18:27:41 -0800

    Initial code drop

---
+++ tests/main.c
@@ -0,0 +1,20 @@
+#include "hashtable/test_hash.h"
+#include "test_bytestr.h"
+#include "test_dl-list.h"
+#include "test_hashtable.h"
+#include "test_list.h"
+#include "test_objectstore.h"
+#include "test_queue.h"
+
+
+int main(){
+    TestCDSByteStr();
+    TestCDSDLList();
+    TestCDSHash();
+    TestCDSHashTable();
+    TestCDSList();
+    TestCDSObjectStore();
+    TestCDSQueue();
+
+    return 0;
+}
