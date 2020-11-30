0a4d397a65341a9272f1ad489914eebbaeb3db9b
stargateaudio <stargateaudio@noreply.github.com>
2020-11-30 06:47:32 -0800

    New data structure: Fast Singly Linked List
    Meant to be used as a disposible, short-lived queue in cases where
    the items are iterated through slowly and cache performance is
    important.

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,7 @@
 #include "hashtable/test_hash.h"
 #include "test_bytestr.h"
 #include "test_dl-list.h"
+#include "test_fast-sll.h"
 #include "test_hashtable.h"
 #include "test_list.h"
 #include "test_objectstore.h"
@@ -10,6 +11,7 @@
 int main(){
     TestCDSByteStr();
     TestCDSDLList();
+    TestCDSFastSLL();
     TestCDSHash();
     TestCDSHashTable();
     TestCDSList();
